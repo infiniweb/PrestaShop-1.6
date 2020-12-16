c0424520c8fb9add3d0d5765fc27998fc3eb319a
infiniweb <17177824+infiniweb@users.noreply.github.com>
2020-12-17 00:59:29 +0400

    Test version of composer

--- travis-scripts/install-prestashop.sh
+++ travis-scripts/install-prestashop.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
 # Clean up needed for StarterTheme tests
-mysql -u root -e "DROP DATABASE IF EXISTS \`prestashop\`;"
-
 echo "* Installing PrestaShop, this may take a while ...";
-php install-dev/index_cli.php --language=en --country=fr --domain=localhost --db_server=127.0.0.1 --db_name=prestashop --db_create=1 --name=prestashop.unit.test --email=demo@prestashop.com --password=prestashop_demo
+/usr/bin/php5.6 install-dev/index_cli.php --language=en --country=fr --domain=localhost --db_server=127.0.0.1 --db_name=prestashopdemo --db_username=homestead --db_password=secret --db_create=1 --name=prestashop.unit.test --email=demo@prestashop.com --password=prestashop_demo
